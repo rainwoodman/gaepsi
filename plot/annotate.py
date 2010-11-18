63b080cb6805956136e37fc7e66d0495c78b469c
Yu Feng <yfeng1@warp.hpc1.cs.cmu.edu>
2010-11-18 10:49:40 -0500

    some more annotation code. needs to be rewritten

--- plot/annotate.py
+++ plot/annotate.py
@@ -1,12 +1,14 @@
 from numpy import empty
-def annotate(field, xrange, yrange, zrange, npixels):
+def annotate(field, xrange, yrange, zrange, npixels, scale):
   pos = field['locations'].copy()
-  mask = pos[:, 0] >= xrange[0]
-  mask &= (pos[:, 0] <= xrange[1])
   pos[:, 0] -= xrange[0]
   pos[:, 1] -= yrange[0]
   pos[:, 0] *= float(npixels[0]) / field.boxsize[0]
   pos[:, 1] *= float(npixels[1]) / field.boxsize[1]
+  mask = (pos[:, 0] >= - scale)
+  mask &= (pos[:, 0] <= (npixels[0]+scale))
+  mask &= (pos[:, 1] >= (-scale))
+  mask &= (pos[:, 1] <= (npixels[1]+scale))
 
   return pos[mask, 0], pos[mask, 1], field['default'][mask]
 
--- tools/pannotate.py
+++ tools/pannotate.py
@@ -5,10 +5,10 @@ from mpi4py import MPI
 from numpy import zeros, array, empty, nonzero, float32
 from pmkfield import pmkfield
 
-def pannotate(comm, snapfile, imagesize, M, ptype, value):
-  field = pmkfield(comm, snapfile, M, ptype, values=[value])
+def pannotate(comm, snapfile, imagesize, M, ptype, valuename, scale=10):
+  field = pmkfield(comm, snapfile, M, ptype, values=[valuename])
 
-  field['default'] = field[value]
+  field['default'] = field[valuename]
 
   strip_px_start = imagesize[0] * comm.rank / (comm.size)
   strip_px_end = imagesize[0] * (comm.rank + 1) / (comm.size)
@@ -18,4 +18,4 @@ def pannotate(comm, snapfile, imagesize, M, ptype, value):
             strip_px_end   * field.boxsize[0] / imagesize[0]]
   yrange = [0, field.boxsize[1]]
   zrange = [0, field.boxsize[2]]
-  return annotate(field, xrange = xrange, yrange = yrange, zrange = zrange, npixels = npixels)
+  return annotate(field, xrange = xrange, yrange = yrange, zrange = zrange, npixels = npixels, scale=scale)
