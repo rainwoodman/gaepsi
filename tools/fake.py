9b3a12c23fefe2c9bc51cde38a4b8f2fa58f1635
Yu Feng <yfeng1@warp.hpc1.cs.cmu.edu>
2011-03-03 15:20:53 -0500

    add a boxsize on the fake field.

--- tools/fake.py
+++ tools/fake.py
@@ -1,10 +1,12 @@
-from field import Field
+from gadget.field import Field
 from numpy import array
 
 
-locations = array([[10.0, 10.0, 10.0], [5,5,10]])
-sml = array([10.0, 10.0])
-value = array([1.0, 1.0])
+field = Field(numpoints=1, components={'mass':'f4', 'vel':('f4', 3), 'sml':'f4'}, boxsize=20)
 
-field = Field(locations = locations, sml = sml, value = value,
-            origin=array([0,0,0]), boxsize=[20,20,20], periodical = False)
+field['locations'][0,:] = array([10.0,10.0,10.0])
+field['vel'][0,:] = array([10.0,20.0,30.0])
+field['sml'][0] = 10.0
+field['mass'][0] = 1.0
+
+
