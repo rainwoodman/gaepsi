7b19bd9ed073a1df4a39d0e29328dcc69d6437e0
Yu Feng <rainwoodman@gmail.com>
2012-10-16 14:34:28 -0400

    redo the cubenoid remapping code.

--- compiledbase/geometry.pyx
+++ compiledbase/geometry.pyx
@@ -1,4 +1,5 @@
 #cython: cdivision=True
+#cython: embedsignature=True
 cimport numpy
 import numpy
 cimport npyiter
@@ -9,212 +10,292 @@ cdef void update_min_max(double value, double * min, double * max) nogil:
   if value < min[0]: min[0] = value
   if value > max[0]: max[0] = value
 
-cdef void constructAABB(double e1[3], double e2[3], double e3[3], double AABB[3][2]) nogil:
-  "construct the AABB of a box given by E a list of edge row vectors"
-  for d in range(3):
-    AABB[d][0] = 0.0
-    AABB[d][1] = 0.0
-    update_min_max(e1[d], &AABB[d][0], &AABB[d][1])
-    update_min_max(e2[d], &AABB[d][0], &AABB[d][1])
-    update_min_max(e3[d], &AABB[d][0], &AABB[d][1])
-    update_min_max(e1[d] + e2[d], &AABB[d][0], &AABB[d][1])
-    update_min_max(e1[d] + e3[d], &AABB[d][0], &AABB[d][1])
-    update_min_max(e2[d] + e3[d], &AABB[d][0], &AABB[d][1])
-    update_min_max(e1[d] + e2[d] + e3[d], &AABB[d][0], &AABB[d][1])
-
-cdef class PeriodicBoundary:
-  """ the perioidc boundary.
-      
-      a boundary can be rotated.
-      then applied to positions, so that they are rotated
-      together, or not, and shifted to any desired boxsize.
-  """
-  cdef readonly numpy.ndarray origin
-  cdef readonly numpy.ndarray edges
+cdef class Rotation:
   cdef readonly numpy.ndarray q
+  cdef readonly numpy.ndarray center
   cdef double (*_q)[3]
-  cdef double * _origin
-  cdef double (*_edges)[3]
-  cdef double _edgenorm[3]
-  cdef int iAABB[3][2]
-
+  cdef double * _center
   def __cinit__(self):
-    self.origin = numpy.empty(3, dtype='f8')
-    self._origin = <double*> self.origin.data
-    self.edges = numpy.empty((3, 3), dtype='f8')
-    self._edges = <double[3] *> self.edges.data
     self.q = numpy.empty((3, 3), dtype='f8')
     self._q = <double[3] *> self.q.data
+    self.center = numpy.empty(3, dtype='f8')
+    self._center = <double*>self.center.data
 
-  def copy(self):
-    return PeriodicBoundary(self.origin, 
-             self.edges[0], self.edges[1], self.edges[2])
-
-  def __init__(self, origin, edge1=None, edge2=None, edge3=None):
-    """ setup the boundary. if edge2 and edge3 .. are None,
-        assume edge1 is a boxsize and the edges are Axis Aligned.
-        use the boxsize as the edge vectors 
-        edge vectors are the translation vectors of the boundary
+  def __init__(self, q=numpy.eye(3), center=(0, 0, 0)):
+    """ q is the rotation on row vectors, dotted from the right 
     """
-    self.origin[:] = origin
-    if edge2 is None and edge3 is None:
-      self.edges[...] = numpy.diag(edge1)
-    else:
-      self.edges[0] = edge1
-      self.edges[1] = edge2
-      self.edges[2] = edge3
+    self.q[...] = q
+    self.center[...] = center
 
-    for d in range(3):
-      self._edgenorm[d] = (self.edges[d] ** 2).sum() ** 0.5
+  def invert(self, X, Y=None, Z=None):
+    if Y is None and Z is None:
+      Z = X[..., 2]
+      Y = X[..., 1]
+      X = X[..., 0]
 
-    # actually the denormed edges. the initial transformation
-    # where the edges are AABB.
-    self.q[...] = numpy.linalg.qr(self.edges)[0]
-
-  def cubenoid(self, matrix):
-    """ cubenoid transformation, 
-        returns the rotation and desired new boxsize,
-        q, boxsize can be then used in apply or shift """
-    if not numpy.allclose(numpy.linalg.det(matrix), 1):
-      raise ValueError('matrix needs to be unitary')
-
-    q, r = numpy.linalg.qr(matrix)
-    # q as row vectors of the old basis in the new basis.
-    # q is the transformation on the right, r is the new boxsize
-    boxsize = numpy.diag(numpy.abs(r)) \
-        * (self._edgenorm[0] \
-        * self._edgenorm[1] \
-        * self._edgenorm[2] ) ** 0.33333333
-    return q, boxsize
-
-
-  def rotate(self, q):
-    """ rotate the boundary by matrix q """
-    self.q[...] = self.q.dot(q)
-    self.edges[...] = self.edges.dot(q)
-
-  def apply(self, X, Y, Z, boxsize, bint apply_rotation=False):
-    """ apply the transformation of the boundry to positions
-        X Y Z and fit all points into boxsize
-        if apply_rotation==False, assume the points X, Y, Z
-        are already rotated.
-    """
-    cdef int iAABB[3][2]
-    cdef double _boxsize[3]
-
-    self.update_iAABB(boxsize, iAABB, _boxsize)
-
-    iter = numpy.nditer([X, Y, Z, None],
-           op_flags=[['readwrite']] * 3 + [['writeonly', 'allocate']],
-           op_dtypes=['f8'] * 3 + ['f8'],
+    iter = numpy.nditer([X, Y, Z],
+           op_flags=[['readwrite']] * 3,
+           op_dtypes=['f8'] * 3,
            flags = ['zerosize_ok', 'external_loop', 'buffered'],
            casting = 'unsafe')
 
     cdef npyiter.CIter citer
     cdef size_t size = npyiter.init(&citer, iter)
     cdef double *x, *y, *z
-   
+
     with nogil:
       while size > 0:
         while size > 0:
           x = <double*>(citer.data[0])
           y = <double*>(citer.data[1])
           z = <double*>(citer.data[2])
-          if apply_rotation:
-            self.rotate_one(x, y, z)
-          (<double*>(citer.data[3]))[0] = self.solve_one(x, y, z, iAABB, _boxsize)
+          self.irotate_one(x, y, z)
+          size = size - 1
+          npyiter.advance(&citer)
+        size = npyiter.next(&citer)
+    return
+
+  def apply(self, X, Y=None, Z=None):
+    if Y is None and Z is None:
+      Z = X[..., 2]
+      Y = X[..., 1]
+      X = X[..., 0]
+     
+    iter = numpy.nditer([X, Y, Z],
+           op_flags=[['readwrite']] * 3,
+           op_dtypes=['f8'] * 3,
+           flags = ['zerosize_ok', 'external_loop', 'buffered'],
+           casting = 'unsafe')
+
+    cdef npyiter.CIter citer
+    cdef size_t size = npyiter.init(&citer, iter)
+    cdef double *x, *y, *z
+
+    with nogil:
+      while size > 0:
+        while size > 0:
+          x = <double*>(citer.data[0])
+          y = <double*>(citer.data[1])
+          z = <double*>(citer.data[2])
+          self.rotate_one(x, y, z)
+          size = size - 1
+          npyiter.advance(&citer)
+        size = npyiter.next(&citer)
+    return
+    
+  cdef void rotate_one(self, double *x, double *y, double *z) nogil:
+    cdef double xx, yy, zz
+    cdef double * _center = self._center
+    x[0] -= _center[0]
+    y[0] -= _center[1]
+    z[0] -= _center[2]
+    xx = x[0] * self._q[0][0] + y[0] * self._q[1][0] + z[0] * self._q[2][0]
+    yy = x[0] * self._q[0][1] + y[0] * self._q[1][1] + z[0] * self._q[2][1]
+    zz = x[0] * self._q[0][2] + y[0] * self._q[1][2] + z[0] * self._q[2][2]
+    x[0] = xx + _center[0]
+    y[0] = yy + _center[1]
+    z[0] = zz + _center[2]
+
+  cdef void irotate_one(self, double *x, double *y, double *z) nogil:
+    cdef double xx, yy, zz
+    cdef double * _center = self._center
+    x[0] -= _center[0]
+    y[0] -= _center[1]
+    z[0] -= _center[2]
+    xx = x[0] * self._q[0][0] + y[0] * self._q[0][1] + z[0] * self._q[0][2]
+    yy = x[0] * self._q[1][0] + y[0] * self._q[1][1] + z[0] * self._q[1][2]
+    zz = x[0] * self._q[2][0] + y[0] * self._q[2][1] + z[0] * self._q[2][2]
+    x[0] = xx + _center[0]
+    y[0] = yy + _center[1]
+    z[0] = zz + _center[2]
+
+cdef class Cubenoid:
+  cdef readonly Rotation rotation
+  cdef readonly numpy.ndarray tries
+  cdef readonly numpy.ndarray oldboxsize
+  cdef readonly numpy.ndarray oldorigin
+  cdef readonly numpy.ndarray newboxsize
+  cdef readonly numpy.ndarray neworigin
+  cdef readonly numpy.ndarray center
+  cdef readonly numpy.ndarray edges
+  cdef double (*_edges)[3]
+
+  def __cinit__(self):
+    self.oldboxsize = numpy.empty(3, 'f8')
+    self.newboxsize = numpy.empty(3, 'f8')
+    self.neworigin = numpy.empty(3, 'f8')
+    self.oldorigin = numpy.empty(3, 'f8')
+    self.center = numpy.empty(3, 'f8')
+    self.edges = numpy.empty((3, 3), dtype='f8')
+    self._edges = <double[3] *> self.edges.data
+
+  def __init__(self, matrix, origin=0, boxsize=1, center=0, neworigin=None):
+    
+    if not numpy.allclose(numpy.linalg.det(matrix), 1):
+      raise ValueError('matrix needs to be unitary')
+
+    self.oldboxsize[:] = boxsize
+    self.oldorigin[:] = origin
+
+    volume = numpy.product(self.oldboxsize)
+    q, r = numpy.linalg.qr(matrix)
+    # q as row vectors of the old basis in the new basis.
+    # q is the transformation on the right, r is the new boxsize
+    self.newboxsize[:] = numpy.diag(numpy.abs(r)) * volume ** 0.33333333
+    self.center[:] = center
+    if neworigin is None:
+      self.neworigin[:] = self.center - self.newboxsize * 0.5
+    else:
+      self.neworigin[:] = neworigin
+
+    self.rotation = Rotation(q, center=self.center)
+    self.edges[:] = numpy.diag(self.oldboxsize).dot(q)
+    self._estimate_search_range()
+
+  def _estimate_search_range(self):
+    cdef numpy.ndarray p = numpy.empty((8, 3), dtype='f8')
+
+    p[0] = self.neworigin
+    p[7] = p[0] + self.newboxsize
+    p[1] = p[0] + self.newboxsize * [0, 0, 1]
+    p[2] = p[0] + self.newboxsize * [0, 1, 0]
+    p[3] = p[0] + self.newboxsize * [1, 0, 0]
+    p[4] = p[0] + self.newboxsize * [0, 1, 1]
+    p[5] = p[0] + self.newboxsize * [1, 1, 0]
+    p[6] = p[0] + self.newboxsize * [1, 0, 1]
+
+    # we never need to try to move more than the AABB of the new bounding box
+    # from the perspective of the old basis.
+    
+    # rotate back to the original coordinate of the AABB climax points
+    #
+    self.rotation.invert(p)
+    p -= self.oldorigin
+    AABBmin = numpy.floor((p.min(axis=0))/ self.oldboxsize)
+    AABBmax = numpy.ceil((p.max(axis=0))/ self.oldboxsize)
+    self.tries = numpy.array(list(self._yield_tries(AABBmin, AABBmax)), dtype='f8', copy=True)
+    
+
+  def _yield_tries(self, AABBmin, AABBmax):
+    oldorigininnew = self.oldorigin.copy()
+    self.rotation.apply(oldorigininnew)
+    tries = numpy.empty(3)
+    tries[0] = AABBmin[0]
+    while tries[0] <= AABBmax[0]:
+      tries[1] = AABBmin[1]
+      while tries[1] <= AABBmax[1]:
+        tries[2] = AABBmin[2]
+        while tries[2] <= AABBmax[2]:
+          rect = numpy.array([ tries, 
+             (tries + [0, 0, 1]),
+             (tries + [0, 1, 0]),
+             (tries + [1, 0, 0]),
+             (tries + [1, 0, 1]),
+             (tries + [1, 1, 0]),
+             (tries + [0, 1, 1]),
+             (tries + [1, 1, 1])])
+          rect = oldorigininnew + rect.dot(self.edges)
+          rectmin = rect.min(axis=0)
+          rectmax = rect.max(axis=0)
+         
+          if not ((rectmax < self.neworigin) | (rectmin > self.neworigin+self.newboxsize)).any():
+          #  print 'a', tries, rectmin, rectmax, origin, origin+boxsize
+            yield tries.copy()
+          else:
+            #print 'r', tries, rectmin, rectmax, origin, origin+boxsize
+            #yield tries.copy()
+            pass
+
+          tries[2] += 1
+        tries[1] += 1
+      tries[0] += 1
+
+  def rotate(self, X, Y=None, Z=None):
+    self.rotation.apply(X, Y, Z)
+
+  def apply(self, X, Y=None, Z=None, without_rotation=False):
+    """ X Y Z and fit all points into boxsize
+        rot is used to determine the range of search with estimate_search_range
+    """
+    cdef double _origin[3]
+    cdef double _boxsize[3]
+
+    cdef double (*_tries)[3]
+    _tries = <double [3]*>self.tries.data
+    cdef int ntries = len(self.tries)
+    # print 'total', len(self.tries)
+    for d in range(3):
+      _origin[d] = self.neworigin[d]
+      _boxsize[d] = self.newboxsize[d]
+
+    cdef bint norot = without_rotation
+    iter = numpy.nditer([X, Y, Z, None],
+           op_flags=[['readwrite']] * 3 + [['writeonly', 'allocate']],
+           op_dtypes=['f8'] * 3 + ['intp'],
+           flags = ['zerosize_ok', 'external_loop', 'buffered'],
+           casting = 'unsafe')
+
+    cdef npyiter.CIter citer
+    cdef size_t size = npyiter.init(&citer, iter)
+    cdef double *x, *y, *z
+    with nogil:
+      while size > 0:
+        while size > 0:
+          x = <double*>(citer.data[0])
+          y = <double*>(citer.data[1])
+          z = <double*>(citer.data[2])
+          if not norot:
+            self.rotation.rotate_one(x, y, z)
+          (<numpy.intp_t*>(citer.data[3]))[0] = self.solve_one(x, y, z, _tries, ntries, _origin, _boxsize)
           size = size - 1
           npyiter.advance(&citer)
         size = npyiter.next(&citer)
     return iter.operands[3]
 
-  cdef void update_iAABB(self, boxsize, int iAABB[3][2], double _boxsize[3]):
-    """ update the integer AABB search limit of the shifts """
-    cdef numpy.ndarray bbinold = numpy.empty((3,3), dtype='f8')
-    cdef double * _bbinold = <double*> bbinold.data
-
-    # in the original coord, the new bounding box
-    # as row vectors, q.T if the old edges are aligned to the old box.
-    # FIXME: if the old edges are not aligned to the old box,
-    # q.T shall not be used, but a qr of edges of some kind.
-    # note that self.q.T gives the cummulated transformation back to
-    # the original AABB system.
-    bbinold[...] = numpy.diag(boxsize).dot(self.q.T)
-    for d in range(3):
-      _boxsize[d] = boxsize[d]
-
-    cdef double AABB[3][2]
-    
-    # we never need to try to move more than the AABB of the new bounding box
-    # from the perspective of the old basis.
-    constructAABB(&_bbinold[0], &_bbinold[3], &_bbinold[6], AABB)
-    for d in range(3):
-      iAABB[d][0] = <int>(floor(AABB[d][0] / self._edgenorm[d]))
-      iAABB[d][1] = <int>(ceil(AABB[d][1] / self._edgenorm[d]))
-      #print iAABB[d][0], iAABB[d][1], AABB[d][0], self._edgenorm[d]
-
-  cdef double badness(self, double pos[3], double boxsize[3]) nogil:
+  cdef double badness(self, double pos[3], double origin[3], double boxsize[3]) nogil:
     cdef double x, badness
     badness = 0
     for d in range(3):
-      x = (pos[d] - self._origin[d]) / boxsize[d]
+      x = (pos[d] - origin[d]) / boxsize[d]
       if x < -1e-5: badness += -x 
       if x >= 1.0 + 1e-5: badness += (x - 1.0)
     return badness
 
-  cdef void rotate_one(self, double *x, double *y, double *z) nogil:
-    cdef double xx, yy, zz
-    xx = x[0] * self._q[0][0] + y[0] * self._q[1][0] + z[0] * self._q[2][0]
-    yy = x[0] * self._q[0][1] + y[0] * self._q[1][1] + z[0] * self._q[2][1]
-    zz = x[0] * self._q[0][2] + y[0] * self._q[1][2] + z[0] * self._q[2][2]
-    x[0] = xx
-    y[0] = yy
-    z[0] = zz
-
-  cdef double solve_one(self, double *x, double *y, double * z, int iAABB[3][2], double boxsize[3]) nogil:
+  cdef int solve_one(self, double *x, double *y, double * z, double (*tries)[3], int ntries, double origin[3], double boxsize[3]) nogil:
     cdef double pos[3]
-    cdef int tries[3]
+    cdef int i
     cdef double badness_min, badness
     cdef double pos_min[3]
+    cdef int i_min = -1
     pos[0] = x[0]
     pos[1] = y[0]
     pos[2] = z[0]
 
-    badness_min = self.badness(pos, boxsize)
+    for i in range(ntries):
+      pos[0] = x[0]
+      pos[1] = y[0]
+      pos[2] = z[0]
+      for d in range(3):
+        pos[0] += tries[i][d] * self._edges[d][0]
+        pos[1] += tries[i][d] * self._edges[d][1]
+        pos[2] += tries[i][d] * self._edges[d][2]
 
-    if badness_min <= 0.0: return badness_min
-
-    tries[0] = iAABB[0][0]
-    while tries[0] <= iAABB[0][1]:
-      tries[1] = iAABB[1][0]
-      while tries[1] <= iAABB[1][1]:
-        tries[2] = iAABB[2][0]
-        while tries[2] <= iAABB[2][1]:
-          pos[0] = x[0]
-          pos[1] = y[0]
-          pos[2] = z[0]
-          for d in range(3):
-            pos[0] += tries[d] * self._edges[d][0]
-            pos[1] += tries[d] * self._edges[d][1]
-            pos[2] += tries[d] * self._edges[d][2]
-
-          badness = self.badness(pos, boxsize)
-          if badness <= 0.0:
-            x[0] = pos[0]
-            y[0] = pos[1]
-            z[0] = pos[2]
-            return badness
-            
-          if badness < badness_min: 
-            pos_min[0] = pos[0]
-            pos_min[1] = pos[1]
-            pos_min[2] = pos[2]
-            badness_min = badness
-          tries[2] += 1
-        tries[1] += 1
-      tries[0] += 1
+      badness = self.badness(pos, origin, boxsize)
+      if badness <= 0.0:
+        x[0] = pos[0]
+        y[0] = pos[1]
+        z[0] = pos[2]
+        return i
+        
+      if i_min == -1 or badness < badness_min: 
+        pos_min[0] = pos[0]
+        pos_min[1] = pos[1]
+        pos_min[2] = pos[2]
+        badness_min = badness
+        i_min = i
 
     x[0] = pos_min[0]
     y[0] = pos_min[1]
     z[0] = pos_min[2]
-    return badness_min
+    return i_min
+
