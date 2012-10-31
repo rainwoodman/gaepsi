4796a47c11df532d876585abe639dadc208aad0c
Yu Feng <rainwoodman@gmail.com>
2012-10-31 10:46:21 -0400

    allow setting header parameters

--- scripts/gadget-dump-header.py
+++ scripts/gadget-dump-header.py
@@ -3,15 +3,22 @@
 from sys import argv
 import argparse
 from gaepsi.snapshot import Snapshot
+
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("-f", dest='format', default='genic', required=False)
   parser.add_argument("filename")
   parser.add_argument("field", default=None, nargs='?')
+  parser.add_argument("-s", dest='value', default=None, required=False)
   args = parser.parse_args()
   snap = Snapshot(args.filename, args.format)
   if args.field is not None:
-    print snap.C[args.field]
+    if args.value is not None:
+      snap.C[args.field] = args.value
+      print args.field, 'set to', snap.C[args.field]
+      snap.save_header()
+    else:
+      print snap.C[args.field]
   else:
     for name in snap.C:
       print name, '=', snap.C[name]
