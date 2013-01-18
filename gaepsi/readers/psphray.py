352395b3a6cd427344513e4dc398e382fbb9067f
Yu Feng <rainwoodman@gmail.com>
2013-01-18 13:05:22 -0500

    fix psphray reader

--- gaepsi/readers/psphray.py
+++ gaepsi/readers/psphray.py
@@ -1,4 +1,4 @@
-class Reader:
+class Snapshot:
     format = 'F'
     header = [
       ('N', ('u4', 6)),
@@ -29,33 +29,37 @@ class Reader:
       ('flag_eos', 'i4'),
       ('unused', ('i4', 4)),
     ]
-    schema = [
-      ('pos', ('f4', 3), [0,4,5], []),
-      ('id', 'u8', [0,4,5], []),
-      ('mass', 'f4', [0,4,5], []),
-      ('ie', 'f4', [0], []),
-      ('rho', 'f4', [0], []),
-      ('ye', 'f4', [0], []),
-      ('xHI', 'f4', [0], []),
-      ('sml', 'f4', [0], []),
-      ('Hmf', 'f4', [0], ['flag_Hmf']),
-      ('Hemf', 'f4', [0], ['flag_Hemf']),
-      ('xHeI', 'f4', [0], ['flag_Helium']),
-      ('xHeII', 'f4', [0], ['flag_Helium']),
-      ('yGdepHI', 'f4', [0], ['flag_dep']),
-#      ('yGdepHeI', 'f4', [0], ['flag_dep']),
-#      ('yGdepHeII', 'f4', [0], ['flag_dep']),
-#      ('yGrecHII', 'f4', [0], ['flag_rec']),
-#      ('yGrecHeII', 'f4', [0], ['flag_rec']),
-#      ('yGrecHeIII', 'f4', [0], ['flag_rec']),
-#      ('HIa_cloudy', 'f4', [0], ['flag_cloudy']),
-#      ('eos', 'f4', [0], ['flag_eos']),
-      ('lasthit', 'i8', [0], []),
-      ('hits', 'i4', [0], []),
-      ('sft', 'f4', [4], []),
-      ('ngammas', 'f8', [5], []),
-      ('spec', 'u8', [5], []),
-    ]
+    class schema:
+      pos = ('f4', 3), [0,4,5], []
+      id = 'u8', [0,4,5], []
+      mass = 'f4', [0,4,5], []
+      ie = 'f4', [0], []
+      rho = 'f4', [0], []
+      ye = 'f4', [0], []
+      xHI = 'f4', [0], []
+      sml = 'f4', [0], []
+      Hmf = 'f4', [0], ['flag_Hmf']
+      Hemf = 'f4', [0], ['flag_Hemf']
+      xHeI = 'f4', [0], ['flag_Helium']
+      xHeII = 'f4', [0], ['flag_Helium']
+      yGdepHI = 'f4', [0], ['flag_dep']
+#      yGdepHeI = 'f4', [0], ['flag_dep']
+#      yGdepHeII = 'f4', [0], ['flag_dep']
+#      yGrecHII = 'f4', [0], ['flag_rec']
+#      yGrecHeII = 'f4', [0], ['flag_rec']
+#      yGrecHeIII = 'f4', [0], ['flag_rec']
+#      HIa_cloudy = 'f4', [0], ['flag_cloudy']
+#      eos = 'f4', [0], ['flag_eos']
+      lasthit = 'i8', [0], []
+      hits = 'i4', [0], []
+      sft = 'f4', [4], []
+      ngammas = 'f8', [5], []
+      spec = 'u8', [5], []
+      __blocks__ = ["pos", "id", "mass", "ie", 
+                    "rho", "ye", "xHI", "sml", "Hmf",  
+                    "Hemf", "xHeI", "xHeII", "yGdepHI", "lasthit", 
+                    "hits", "sft", "ngammas", "spec" ]
+
     class constants:
       OmegaB = 0.044
       PhysDensThresh = 0.000831188
