09bb5b45ef01bc5196cd26370ff0d3b08be66530
Yu Feng <yfeng1@coma.hpc1.cs.cmu.edu>
2013-04-17 14:10:28 -0400

    cic and powerspectrum correlation function.

--- gaepsi/tools/analyze.py
+++ gaepsi/tools/analyze.py
@@ -132,27 +132,48 @@ class BHDetail:
      ('mainid', 'u8'),
      ('z', 'f8'),
     ]
+
+    if filename[-4:] == '.npy':
+        self.data = numpy.load(filename)
+        return
+
     if numfields is None:
       numfields = len(file(filename).readline().split())
     rawdt = numpy.dtype(dtlist[:numfields])
     raw = numpy.loadtxt(filename, dtype=rawdt)
     data = numpy.empty(len(raw), dtlist)
     data[...] = raw
+    del raw
     data['mainid'] = data['id']
     data['z'] = 1 / data['time'] - 1
+
+    self.data = data
+
     if mergerfile is not None:
       merger = numpy.loadtxt(mergerfile, dtype=[('time_q', 'f8'), 
            ('after', 'u8'), ('swallowed', 'u8')], ndmin=1)
-      while True:
-        changed = False
-        for time_q, after, swallowed in merger:
-          changed = changed or (data['mainid'] == swallowed).any()
-          data['mainid'][[data['mainid'] == swallowed]] = after
-        if not changed: break
-  
-    data.sort(order=('mainid', 'time'))
-    self.data = data
+      while self.fixmainid(merger) > 0:
+        pass
 
+  def fixmainid(self, merger):
+    arg = sharedmem.argsort(self.data['mainid'])
+    self.data[...] = self.data[arg]
+
+    left = self.data['mainid'].searchsorted(merger['swallowed'], side='left')
+    right = self.data['mainid'].searchsorted(merger['swallowed'], side='right')
+    mask = self.data['mainid'][left] == merger['swallowed']
+    print 'fixing mainid', mask.sum(), 'remaining'
+    left = left[mask]
+    right = right[mask]
+    for i, row in enumerate(merger[mask]):
+      time, after, swallowed = row
+      l = left[i]
+      r = right[i]
+      self.data['mainid'][l:r+1] = after
+    return mask.sum()
+
+  def save(self, filename):
+    numpy.save(filename, self.data)
   def mainid(self, id):
     return self['mainid'][self['id'] == id][0]
 
@@ -201,3 +222,240 @@ class BHDetail:
       from matplotlib import pyplot
       ax = pyplot.gca()
     return ax.plot(xdata[mask], property[mask], *args, **kwargs)
+
+
+def cic(pos, Nmesh, boxsize, weights=1.0, dtype='f8'):
+    """ CIC approximation from points to Nmesh,
+        each point has a weight given by weights.
+        This does not give density.
+        pos is supposed to be row vectors. aka for 3d input
+        pos.shape is (?, 3).
+
+    """
+    chunksize = 1024 * 16
+    BoxSize = 1.0 * boxsize
+    Ndim = pos.shape[-1]
+    Np = pos.shape[0]
+    dtype = numpy.dtype(dtype)
+    mesh = numpy.zeros(shape=(Nmesh, ) * Ndim, 
+            dtype=dtype, order='C')
+    flat = mesh.reshape(-1)
+    neighbours = ((numpy.arange(2 ** Ndim)[:, None] >> \
+            numpy.arange(Ndim)[None, :]) & 1)
+    for start in range(0, Np, chunksize):
+        chunk = slice(start, start+chunksize)
+        if numpy.isscalar(weights):
+          wchunk = weights
+        else:
+          wchunk = weights[chunk]
+        gridpos = pos[chunk] * (Nmesh / BoxSize)
+        intpos = numpy.intp(gridpos)
+        for i, neighbour in enumerate(neighbours):
+            neighbour = neighbour[None, :]
+            targetpos = intpos + neighbour
+            targetindex = numpy.ravel_multi_index(
+                    targetpos.T, mesh.shape, mode='wrap')
+            kernel = (1.0 - numpy.abs(gridpos - targetpos)).prod(axis=-1)
+            add = wchunk * kernel
+            u, label = numpy.unique(targetindex, return_inverse=True)
+            flat[u] += numpy.bincount(label, add)
+    return mesh
+
+def collapse(field, ticks, axis, logscale=False):
+    """ collapse axis of a field,
+        tics are the coordinates of the axes.
+        tics is of length field.shape
+        and len(tics[i]) == field.shape[i]
+        
+        axis is a list of axis to collapse.
+        the collapsed axis will be represented by one
+        axis added to the end of the axes of the return
+        value.
+        returns
+
+           tics, newfield
+        where tics are the coordinates of the new field.
+    """
+    Ndim = len(field.shape)
+    for i in range(Ndim):
+      assert len(ticks[i]) == field.shape[i]
+
+    if axis is None or len(axis) == 0:
+        return ticks, field
+
+    axis = list(axis)
+
+    preserve = []
+    newticks = []
+    dist = None
+    for i in range(Ndim):
+        if i in axis:
+            if dist is None:
+                dist = ticks[i] ** 2
+            else:
+                dist = dist[:, None] + ticks[i][None, :] ** 2
+            dist.shape = -1
+        else:
+            preserve.append(i)
+            newticks.append(ticks[i])
+    dist **= 0.5
+    dmin = dist[dist > 0].min()     
+    dmax = dist.max()
+
+    Nbins = numpy.max(numpy.array(field.shape)) + 1
+    if not logscale:
+        bins = numpy.linspace(dmin, dmax, Nbins, endpoint=True)
+        center = 0.5 * (bins[1:] + bins[:-1])
+    else:
+        ldmin, ldmax = numpy.log10([dmin, dmax])
+        bins = numpy.logspace(ldmin, ldmax, Nbins, endpoint=True)
+        center = (bins[1:] * bins[:-1]) ** 0.5
+
+    newticks.append(center)
+
+    slabs = field.transpose(preserve + axis).reshape(-1, len(dist))
+
+    dig = numpy.digitize(dist, bins)
+    suminv = 1.0 / numpy.bincount(dig, weights=dist, minlength=bins.size+1)[1:-1]
+
+    newfield = numpy.empty((slabs.shape[0], len(center)))
+
+    for i, slab in enumerate(slabs):
+        kpk = dist * slab
+        kpksum = numpy.bincount(dig, weights=kpk, minlength=bins.size+1)[1:-1]
+        newfield[i] = kpksum * suminv
+
+    newfield.shape = [field.shape[i] for i in preserve] + [len(center)]
+
+    return newticks, newfield
+
+def powerfromdelta(delta, boxsize, logscale=False, collapse=None):
+    """ delta is over density.
+        this obviously does not correct for redshift evolution.
+        returns a collapsed powerspectrum, 
+
+        if collapse is None, collapse all dimensions.
+        if collapse is not None, just collapse the selecte dimensions.
+        if collapse is False, do not collapse and return the full n-dim
+        Pk.
+
+        returns k, P
+
+        where k is a list of the bin centers.
+           k[0] is the bins centers of the first uncollapsed dimension,
+           k[-1] is the centers of the collapsed dimensions,
+           when collapse is None, k is just the center of the collapsed
+           dimensions.
+        P is of shape (uncollapsed ... axis, len(k))
+    
+       The power spectrum is assumed to have the gadget convention,
+       AKA, normalized to (2 * pi) ** -3 times sigma_8.
+    """
+    Dplus = 1.0 # no evolution correction
+  
+    N = numpy.prod(delta.shape, dtype='f8')
+    Ndim = len(delta.shape)
+    BoxSize = numpy.empty(Ndim, dtype='f8')
+    BoxSize[:] = boxsize
+  
+    # each dim has a different K0
+    K0 = 2 * numpy.pi / BoxSize
+  
+    delta_k = numpy.fft.rfftn(delta) / N
+    intK = numpy.ogrid[[slice(0, n) for n in delta_k.shape]]
+  
+    half = numpy.array(delta_k.shape) // 2
+    # last dim is already halved
+    half[-1] = delta_k.shape[-1]
+  
+    if collapse is None:
+        collapse = range(Ndim)
+    if collapse == False:
+        collapse = []
+    collapse = list(collapse)
+    original = []
+    Kclps = 0
+    Kret = []
+    for i in range(Ndim):
+        kx = (half[i] - numpy.abs(half[i] - intK[i])) * K0[i]
+        if i in collapse:
+            Kclps = Kclps + kx ** 2
+        else:
+            Kret.append(kx.ravel().copy())
+            original.append(i)
+
+    if len(collapse) == 0:
+        return Kret, numpy.abs(delta_k) ** 2 * K0.prod() ** -1 * Dplus ** 2
+
+    #this shall be calling collapse instead!
+    # now we bin Kclps
+    Kclps = Kclps ** 0.5
+    kmax = Kclps.max() 
+    kmin = Kclps[Kclps > 0].min()
+    if not logscale:
+        kbins = numpy.linspace(kmin, kmax, numpy.max(half), endpoint=True)
+        kcenter = 0.5 * (kbins[1:] + kbins[:-1])
+    else:
+        lkmin, lkmax = numpy.log10([kmin, kmax])
+        kbins = numpy.logspace(lkmin, lkmax, numpy.max(half), endpoint=True)
+        kcenter = (kbins[1:] * kbins[:-1]) ** 0.5
+
+    Kret.append(kcenter)
+
+    slabs = delta_k.transpose(original + collapse).reshape([-1] + 
+            [delta_k.shape[i] for i in collapse])
+    Kclps = Kclps.transpose(original + collapse).reshape(
+            [Kclps.shape[i] for i in collapse])
+
+    dig = numpy.digitize(Kclps.ravel(), kbins)
+    ksuminv = 1.0 / numpy.bincount(dig, weights=Kclps.ravel(), minlength=kbins.size+1)[1:-1]
+
+    pk = numpy.empty((slabs.shape[0], len(kcenter)))
+
+    for i, slab in enumerate(slabs):
+        kpk = Kclps * (numpy.abs(slab) ** 2) # * K0 ** -3 * Dplus ** 2
+        kpksum = numpy.bincount(dig, weights=kpk.ravel(), minlength=kbins.size+1)[1:-1]
+        pk[i] = kpksum * ksuminv
+
+    pk = pk.reshape([delta_k.shape[i] for i in original] + [pk.shape[-1]])
+
+    if len(Kret) == 1: 
+        Kret = Kret[0]
+
+    return Kret, pk * K0.prod() ** -1 * Dplus ** 2
+
+def corrfrompower(K, P, logscale=False, R=None):
+    """calculate correlation function from power spectrum,
+       P is 1d powerspectrum. if R is not None, estimate at
+       those points .
+
+       returns R, xi(R)
+
+       internally this does a numerical integral with the trapz
+       rule for the radial direction of the fourier transformation,
+       with a gaussian damping kernel (learned from Xiaoying) 
+       the nan points of P is skipped. (usually when K==0, P is nan)
+
+       input power spectrum is assumed to have the gadget convention,
+       AKA, normalized to (2 * pi) ** -3 times sigma_8.
+
+       The integral can also be done on log K instead of K if logscale 
+       is True.
+    """
+    mask = ~numpy.isnan(P) & (K > 0)
+    K = K[mask]
+    P = P[mask]
+    P = P * (2 * numpy.pi) ** 3 # going from GADGET to xiao
+    if R is None:
+      R = 2 * numpy.pi / K
+    if logscale:
+      weight = K * numpy.exp(-K**2)
+      diff = numpy.log(K)
+    else:
+      weight = numpy.exp(-K**2)
+      diff = K
+    XI = [4 * numpy.pi / r * \
+        numpy.trapz(P * numpy.sin(K * r) * K * weight, diff) for r in R]
+    XI = (2 * numpy.pi) ** -3 * numpy.array(XI)
+  
+    return R, XI
